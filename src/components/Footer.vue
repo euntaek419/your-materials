908142689e7ead6ec4f41ee457fcb60320e76b47
euntaek419 <cet4713@gmail.com>
2023-05-31 00:43:38 +0900

    2023/05/31_Footer 수정

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="footer">
+
     <div class="footer_box">
-      <div>{{ ItemData[0].name }}</div>
+      <button class="footer_btn" style="color: #b73aff; background-color: #f4dfff; width: 120px;">
+        {{ ItemData[0].name }}
+      </button>
+    </div>
 
-      <div>{{ ItemData[1].name }}</div>
+    <div class="footer_box">
+      <button class="footer_btn" style="color: #3680ff; background-color: #e1ecff; width: 120px;">{{ ItemData[1].name }}</button>
+    </div>
+    
+    <div class="footer_box">
+      <button class="footer_btn2" style="color: #ff9c00; background-color: #ffe6bd; width: 80px;">{{ ItemData[2].subname }}</button>   
+      <button class="footer_btn">폭룡왕의 인장</button>
+      <button class="footer_btn">용의 겁화</button>
+    </div>
 
-      <div>{{ ItemData[2].subname }}</div>
-
-      <div>{{ ItemData[3].subname }}</div>
+    <div class="footer_box">
+      <button class="footer_btn3" style="color: #ff3535; background-color: #ffdfdf; width: 150px;">
+        {{ ItemData[3].subname }}
+      </button>
+      <button class="footer_btn">폭룡왕의 인장</button><p></p>
+      <button class="footer_btn">용의 겁화</button>
 
     </div>
+
   </div>
 </template>
 
@@ -22,9 +38,42 @@ export default {
 </script>
 
 <style>
-
 .footer {
   height: 150px;
+  display: flex;
+}
+
+.footer_box{
+  width: 400px;
+}
+
+.footer_btn{
+  height: 35px;
+  border-radius: 10px;
+  cursor: pointer;
   font-size: 20px;
+  border: none;
+  outline: none;
+  text-align: center;
+}
+
+.footer_btn2{
+  height: 35px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  text-align: center;
+}
+
+.footer_btn3{
+  height: 35px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  text-align: center;
 }
 </style>
