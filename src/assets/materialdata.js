9affcb29ffa43d12ca23a635489f4642fa23656a
euntaek419 <cet4713@gmail.com>
2023-06-27 18:30:19 +0900

    2023/06/27_차원회랑 획득 갯수가 비정상적으로 많은 오류 수정

--- src/assets/materialdata.js
+++ src/assets/materialdata.js
@@ -14,7 +14,7 @@ export default [
         name : '차원회랑',
         itemname : '흐릿한 기억의 조각',
         buyneed : 360, // 차원회랑 필요 재료
-        getitem : 54, // 3단 클리어시 획득 재료
+        getitem : 24, // 3단 클리어시 획득 재료
         shareitem : 36,
         getpc : 3,
         getpass : 3,
